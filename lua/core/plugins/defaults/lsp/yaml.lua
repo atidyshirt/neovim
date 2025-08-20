3bab1d7f1a43937bcf16805f1a5539cf4ed0cc57
jordanp <jordanpyott@gmail.com>
2025-08-21 00:30:20 +1200

    MAINT: Add kubernetes to bootstrap in schemas

--- lua/core/plugins/defaults/lsp/yaml.lua
+++ lua/core/plugins/defaults/lsp/yaml.lua
@@ -1,6 +1,24 @@
 local util = require "core.util"
 
+vim.api.nvim_create_user_command("DiagnosticsWithSource", function()
+  local diagnostics = vim.diagnostic.get(0)  -- get diagnostics in current buffer
+  for _, diag in ipairs(diagnostics) do
+    print(string.format("Line %d: %s (Source: %s)", diag.lnum + 1, diag.message, diag.source or "unknown"))
+  end
+end, {})
+
 return {
+  {
+    'diogo464/kubernetes.nvim',
+    opts = {
+      schema_strict = true,
+      schema_generate_always = true,
+      patch = true,
+      yamlls_root = function()
+        return vim.fs.joinpath(vim.fn.stdpath("data"), "/mason/packages/yaml-language-server/")
+      end
+    }
+  },
   {
     "cwrau/yaml-schema-detect.nvim",
     ---@module "yaml-schema-detect"
@@ -26,6 +44,10 @@ return {
             new_config.settings.yaml.schemas = vim.tbl_deep_extend(
               "force",
               new_config.settings.yaml.schemas or {},
+              {
+                [require('kubernetes').yamlls_schema()] = "*.yaml",
+                [require('kubernetes').yamlls_schema()] = require('kubernetes').yamlls_filetypes()
+              },
               require("schemastore").yaml.schemas()
             )
           end,
