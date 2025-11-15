a71055de39e5a18828902c24cf2f9da794f5a9cf
jordanp <jordanpyott@gmail.com>
2025-11-15 13:23:17 +1300

    chore(markdown): Fix markup rendering issue with conceal level

--- lua/core/plugins/ui/markdown.lua
+++ lua/core/plugins/ui/markdown.lua
@@ -1,11 +1,23 @@
 return {
-    {
-        "OXY2DEV/markview.nvim",
-        lazy = false,
-        opts = {
-            preview = {
-                icon_provider = "devicons",
-            }
-        }
+  {
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
     },
+    config = function()
+      require("markview").setup({
+        preview = {
+          modes = { "n", "no", "i", "c" },
+          hybrid_modes = { "i", "n" },
+          callbacks = {
+            on_enable = function(_, win)
+              vim.wo[win].conceallevel = 2
+              vim.wo[win].concealcursor = "nc"
+            end,
+          },
+        },
+      })
+    end,
+  },
 }
