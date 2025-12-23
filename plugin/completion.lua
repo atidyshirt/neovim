207b0d9001fe175af13836a593f1e1f426107e1c
jordanp <jordanpyott@gmail.com>
2025-12-24 12:53:41 +1300

    fix(copilot): fix NES integration to allow for accept/abort of suggestions

--- plugin/completion.lua
+++ plugin/completion.lua
@@ -1,89 +1,94 @@
 vim.pack.add({
-  { src = "https://github.com/hrsh7th/nvim-cmp" },
-  { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
-  { src = "https://github.com/hrsh7th/cmp-buffer" },
-  { src = "https://github.com/hrsh7th/cmp-path" },
-  { src = "https://github.com/L3MON4D3/LuaSnip" },
+    { src = "https://github.com/hrsh7th/nvim-cmp" },
+    { src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+    { src = "https://github.com/hrsh7th/cmp-buffer" },
+    { src = "https://github.com/hrsh7th/cmp-path" },
+    { src = "https://github.com/L3MON4D3/LuaSnip" },
 })
 
 local cmp = require("cmp")
 local luasnip = require("luasnip")
 
 cmp.setup({
-  completion = {
-    autocomplete = { cmp.TriggerEvent.TextChanged },
-    completeopt = "menu,menuone,noinsert",
-  },
+    completion = {
+        autocomplete = { cmp.TriggerEvent.TextChanged },
+        completeopt = "menu,menuone,noinsert",
+    },
 
-  performance = {
-    fetching_timeout = 50,
-    max_view_entries = 20,
-  },
+    performance = {
+        fetching_timeout = 50,
+        max_view_entries = 20,
+    },
 
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
 
-  mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end),
+    mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            elseif luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
 
-    ["<C-j>"] = cmp.mapping(function()
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        cmp.complete()
-      end
-    end),
+        ["<C-e>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.abort()
+            else
+                fallback()
+            end
+        end),
 
-    ["<CR>"]  = cmp.mapping.confirm({ select = true }),
-    ["<C-l>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<C-k>"] = cmp.mapping.select_prev_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-  }),
+        ["<C-j>"] = cmp.mapping(function()
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                cmp.complete()
+            end
+        end),
 
-  sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "path" },
-    { name = "buffer" },
-  }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-l>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    }),
 
-  window = {
-    documentation = cmp.config.window.bordered(),
-  },
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+        { name = "buffer" },
+    }),
 
-  formatting = {
-    fields = { "abbr", "menu", "kind" },
-  },
+    window = {
+        documentation = cmp.config.window.bordered(),
+    },
+
+    formatting = {
+        fields = { "abbr", "menu", "kind" },
+    },
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 vim.lsp.config("*", { capabilities })
-
--- Removed: Entire NES normal mode <Tab> keymap
 
 vim.opt.completeopt = { "menu", "menuone", "noinsert" }
 vim.opt.shortmess:append("c")
