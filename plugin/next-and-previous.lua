4abcb49b06e98de88839d99f7590e02351dd59c9
jordanp <jordanpyott@gmail.com>
2025-12-26 12:51:18 +1300

    fix: next-and-previous correctly doing next and previous behaviour
    prefixes must be different to ensure prev/next don't map to the same
    value (kind of a bug in my opinion, I would prefer a single prefix key,
    and then a mapping for c-n/p based on `<prefix>-<command>`, but I can't
    be bothered fixing it.

--- plugin/next-and-previous.lua
+++ plugin/next-and-previous.lua
@@ -1,26 +1,13 @@
 vim.pack.add({
-  { src = "https://github.com/liangxianzhe/nap.nvim", commit = "925921b" },
+  { src = "https://github.com/liangxianzhe/nap.nvim" },
 })
 
 require("nap").setup({
+  -- Note: overriding ,; here because I never use them in favour of using highlights on 'f,t' commands
   next_prefix = ",",
-  prev_prefix = ",",
+  prev_prefix = ";",
   next_repeat = "<c-n>",
   prev_repeat = "<c-p>",
 })
 
-require("nap").operator("h", {
-  next = {
-    command = function()
-      require("gitsigns").next_hunk({ preview = false, wrap = true })
-    end,
-    desc = "Next diff",
-  },
-  prev = {
-    command = function()
-      require("gitsigns").prev_hunk({ preview = false, wrap = true })
-    end,
-    desc = "Prev diff",
-  },
-  mode = { "n", "v", "o" },
-})
+require("nap").map('h', require("nap").gitsigns())
