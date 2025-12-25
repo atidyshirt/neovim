4c44b0b666cd53dd36d5f301a6504d3ab2994097
jordanp <jordanpyott@gmail.com>
2025-12-26 12:08:54 +1300

    feat(cloak): Add cloak.nvim plugin for environment variables

--- plugin/utilities.lua
+++ plugin/utilities.lua
@@ -5,6 +5,24 @@ vim.pack.add({
   { src = "https://github.com/vitalk/vim-shebang" },
   { src = "https://github.com/mtdl9/vim-log-highlighting" },
   { src = "https://github.com/numToStr/Comment.nvim" },
+  { src = "https://github.com/laytan/cloak.nvim" },
 })
 
-require("Comment").setup({})
+require("Comment").setup()
+
+require('cloak').setup({
+  enabled = true,
+  cloak_character = '*',
+  highlight_group = 'Comment',
+  cloak_length = nil,
+  try_all_patterns = true,
+  cloak_telescope = true,
+  cloak_on_leave = false,
+  patterns = {
+    {
+      file_pattern = { '.env*', '*.tfvars' },
+      cloak_pattern = { '=.+', ':.+' },
+      replace = nil,
+    },
+  },
+})
