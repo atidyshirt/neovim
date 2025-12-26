48048797805328f6e196754fc927da3d477716ef
jordanp <jordanpyott@gmail.com>
2025-12-26 13:36:00 +1300

    fix: fix completion s.t ctrl+j/k opens completion if its closed

--- plugin/completion.lua
+++ plugin/completion.lua
@@ -31,23 +31,23 @@ cmp.setup({
     },
 
     mapping = cmp.mapping.preset.insert({
-        ["<C-j>"] = cmp.mapping(function(fallback)
+        ["<C-j>"] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             elseif luasnip.expand_or_locally_jumpable() then
                 luasnip.expand_or_jump()
             else
-                fallback()
+                cmp.complete()
             end
         end, { "i", "s" }),
 
-        ["<C-k>"] = cmp.mapping(function(fallback)
+        ["<C-k>"] = cmp.mapping(function()
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.locally_jumpable(-1) then
                 luasnip.jump(-1)
             else
-                fallback()
+                cmp.complete()
             end
         end, { "i", "s" }),
 
