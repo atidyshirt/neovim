fd48171c826ba7968882a7cc985f1385897f536f
jordanp <jordanpyott@gmail.com>
2026-01-04 22:38:56 +1300

    fix(copilot): remove clashing keybind causing block when typing text
    This patch removes a clashing terminal keymapping on the leader key that
    would cause pauses when typing in terminal mode windows (pause when
    pressing space key due to a `<space>at` key binding)

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -15,7 +15,7 @@ vim.keymap.set({ "n" }, "<leader>aa", function()
   require("sidekick").nes_jump_or_apply()
 end, { expr = true, desc = "jump or apply suggestion" })
 
-vim.keymap.set({ "n", "t", "x" }, "<leader>at", function()
+vim.keymap.set({ "n", "x" }, "<leader>ai", function()
   require("sidekick.cli").toggle()
 end, { desc = "Sidekick Toggle" })
 
@@ -26,7 +26,3 @@ end, { desc = "Select CLI" })
 vim.keymap.set({ "x" }, "<leader>av", function()
   require("sidekick.cli").send({ msg = "{selection}" })
 end, { desc = "Send Visual Selection" })
-
-vim.keymap.set({ "n" }, "<leader>ai", function()
-  require("sidekick.cli").toggle({ name = "copilot", focus = true })
-end, { desc = "Sidekick Toggle Copilot" })
